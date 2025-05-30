b67d8e71ef99927f80d790c335a33a079a9cb213
master <fhdlwp15@naver.com>
2025-05-30 15:54:04 +0900

    n+1 문제 해결

--- KS/src/main/java/com/example/ks/computer/repository/ComputerRepository.java
+++ KS/src/main/java/com/example/ks/computer/repository/ComputerRepository.java
@@ -1,8 +1,13 @@
 package com.example.ks.computer.repository;
 
 import com.example.ks.computer.domain.Computer;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ComputerRepository extends JpaRepository<Computer, Integer> {
+    @EntityGraph(attributePaths = "department")
+    List<Computer> findAll();
 }
