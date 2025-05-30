d82837b48e4388361e8db11f8c3c49b5868c4bcd
master <fhdlwp15@naver.com>
2025-05-30 15:54:04 +0900

    n+1 문제 해결

--- KS/src/main/java/com/example/ks/monitor/repository/MonitorRepository.java
+++ KS/src/main/java/com/example/ks/monitor/repository/MonitorRepository.java
@@ -1,7 +1,12 @@
 package com.example.ks.monitor.repository;
 
 import com.example.ks.monitor.domain.Monitor;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MonitorRepository extends JpaRepository<Monitor, Integer> {
+    @EntityGraph(attributePaths = "department")
+    List<Monitor> findAll();
 }
